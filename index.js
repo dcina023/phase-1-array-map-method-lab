c1d048520f95c594c31ed6e780dc9fb1a9c8aaa7
+ <+>
2026-05-10 15:44:46 -0400

    complete array map method lab

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+
+
+function titleCased() {
+  return tutorials.map((title) => {
+    let result = "";
+
+    for (let item = 0; item < title.length; item++) {
+      if (item === 0 || title[item - 1] === " ") {
+        result += title[item].toUpperCase();
+      } else {
+        result += title[item]
+      }
+    }
+
+    return result
+  });
 }
